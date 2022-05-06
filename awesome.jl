17b5f9538ba1fc0eb07762df461700392e461fd2
Olivier Labayle <olabayle@gmail.com>
2022-05-06 15:11:15 +0100

    add Part 2

---
+++ awesome.jl
@@ -0,0 +1,21 @@
+###############################################################################
+#####                      AWESOME ALGORITHM                              #####
+###############################################################################
+
+"""
+    AwsomeAlgorithm(x)
+
+This algorithm takes as input an array. It sums up all the entries with 
+the additional constraint that each unique element must be used at most twice.
+
+Example:
+AwsomeAlgorithm([1, 2, 4, 4, 4, 1]) = 1 + 1 + 2 + 4 + 4 = 12
+
+Once it is done, can you make it work for multiple arrays?
+
+AwsomeAlgorithm([1, 2, 4, 4, 4, 1], [1 2 2; 3 4 4; 2 4 3]) = (12, 19)
+"""
+
+function AwsomeAlgorithm
+    # You can do better than that!
+end
