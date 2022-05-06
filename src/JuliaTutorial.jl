9c5a70cc58876df33e01c77fd99a1ae2b7963576
Olivier Labayle <olabayle@gmail.com>
2022-05-06 15:33:55 +0100

    update

--- src/JuliaTutorial.jl
+++ src/JuliaTutorial.jl
@@ -8,12 +8,12 @@ Structures are how you can define new types
 struct ModelThatIAmProudOf end
 
 """
-This is your ambitious fitting procedure
+This is your ambitious fitting procedure and should return at least a `fitresult` object.
 """
 function fit(model, X, y) end
 
 """
-This should probably output ŷ
+This should output ŷ, a prediction for the new X.
 """
 function predict(model, fitresult, X) end
 
